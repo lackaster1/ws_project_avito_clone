615c2a5a097a539b0ca1a09c1702084a9a8ca200
Sviatoslav Kravchenko <sviatoslav.kravchenko@team.wrike.com>
2019-04-04 17:29:30 +0300

    Add modal

--- genderRouletteScript.js
+++ genderRouletteScript.js
@@ -1,6 +1,26 @@
-$(".hover-img-wrapper").click(function () {
-    var a = confirm("You must buy rubber woman. Go to Avito?");
-    if (a==true) {
-        window.location.href="http://avito.ru/additem"
-    }
+$(document).ready(function () {
+    var overlay = $('#overlay');
+    var open_modal = $('.basic-gender-container');
+    var close_modal = $('.close-button-container, #overlay');
+    var modal = $('.modal-form-main-container');
+
+    open_modal.click(function (event) {
+        event.preventDefault();
+        overlay.fadeIn(400,
+            function () {
+                modal
+                    .css('display', 'block', 'opacity')
+                    .animate({opacity: 1, top: '50%'}, 100);
+            });
+    });
+
+    close_modal.click(function () {
+        modal
+            .animate({opacity: 0, top: '45%'}, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
 });
