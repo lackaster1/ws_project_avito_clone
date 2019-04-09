6d21466a5c8a6564b49c64cb4cfdd0d7fe8e7f52
Sviatoslav Kravchenko <sviatoslav.kravchenko@team.wrike.com>
2019-04-09 18:24:45 +0300

    Add new function

--- genderRouletteScript.js
+++ genderRouletteScript.js
@@ -34,54 +34,67 @@ class Gender {
   }
 
   static createGender() {
-    return new Gender()
+    return new Gender();
   }
 }
 
-var genders = ["brooding duck", "mighty kangaroo", "shocked horse", "crying baby", "russian boy", "kurly kurly", "double finger bird", "eagle of anal freedom"];
+$(document).ready(function () {
+  var start = $('.start-roulette-button');
+  var genders = [gen00(), gen01(), gen02(), gen03(), gen04(), gen05(), gen06(), gen07(), gen08()];
+  start.click(function () {
+    /*    document.body.onload = addElement;
 
+        function addElement () {
+          var newDiv = document.createElement("div");
+          var newContent = document.createTextNode(genders[randomInteger(0,8)]);
+          newDiv.appendChild(newContent);
 
+          var currentDiv = document.getElementsByClassName("simple-container");
+          document.body.insertBefore(newDiv,currentDiv);
+        }*/
+    console.log(genders[randomInteger(0, 8)]);
 
+  });
 
-var mighty_kangaroo = {
-  id: 0,
-  name: "mighty kangaroo",
-  img_link: "https://images-na.ssl-images-amazon.com/images/I/71zPg7nI4DL._SL1500_.jpg",
-  description: "Best gender ever!"
-}
-var shocked_horse = {
-  name: "shocked horse",
-  img_link: "https://images-na.ssl-images-amazon.com/images/I/71O2blC076L._UX679_.jpg",
-  description: "Best gender ever!"
-}
-var crying_baby = {
-  name: "crying baby",
-  img_link: "https://images-na.ssl-images-amazon.com/images/I/61Pi1yg8%2BLL._UX679_.jpg",
-  description: "Best gender ever!"
-}
-var russian_boy = {
-  name: "russian boy",
-  img_link: "https://images-na.ssl-images-amazon.com/images/I/61uNk%2BCM7VL._UX679_.jpg",
-  description: "Best gender ever!"
-}
-var kurly_kurly = {
-  name: "kurly kurly",
-  img_link: "https://images-na.ssl-images-amazon.com/images/I/719%2BEaf8sDL._UX679_.jpg",
-  description: "Best gender ever!"
-}
-var double_finger_bird = {
-  name: "double finger bird",
-  img_link: "https://images-na.ssl-images-amazon.com/images/I/7136vN9tAQL._UX679_.jpg",
-  description: "Best gender ever!"
-}
-var eagle_of_anal_freedom = {
-  name: "eagle of anal freedom",
-  img_link: "https://images-na.ssl-images-amazon.com/images/I/71mfn6d-G2L._SX679_.jpg",
-  description: "Best gender ever!"
-}
-var big_hairy_kitty = {
-  name: "eagle of anal freedom",
-  img_link: "https://images-na.ssl-images-amazon.com/images/I/71mfn6d-G2L._SX679_.jpg",
-  description: "Best gender ever!"
+  function randomInteger(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
+});
+
+function gen00() {
+  return new Gender("0", "brooding duck", "https://images-na.ssl-images-amazon.com/images/I/61QBy1vqeML._UX679_.jpg", "Best gender ever!");
 }
 
+function gen01() {
+  return new Gender("1", "mighty kangaroo", "https://images-na.ssl-images-amazon.com/images/I/61HQmuFnn1L._UX679_.jpg", "Best gender ever!");
+}
+
+function gen02() {
+  return new Gender("2", "shocked horse", "https://images-na.ssl-images-amazon.com/images/I/71zPg7nI4DL._SL1500_.jpg", "Best gender ever!");
+}
+
+function gen03() {
+  return new Gender("3", "crying baby", "https://images-na.ssl-images-amazon.com/images/I/71O2blC076L._UX679_.jpg", "Best gender ever!");
+}
+
+function gen04() {
+  return new Gender("4", "russian boy", "https://images-na.ssl-images-amazon.com/images/I/61Pi1yg8%2BLL._UX679_.jpg", "Best gender ever!");
+}
+
+function gen05() {
+  return new Gender("5", "kurly kurly", "https://images-na.ssl-images-amazon.com/images/I/61uNk%2BCM7VL._UX679_.jpg", "Best gender ever!");
+}
+
+function gen06() {
+  return new Gender("6", "double finger bird", "https://images-na.ssl-images-amazon.com/images/I/719%2BEaf8sDL._UX679_.jpg", "Best gender ever!");
+}
+
+function gen07() {
+  return new Gender("7", "eagle of anal freedom", "https://images-na.ssl-images-amazon.com/images/I/7136vN9tAQL._UX679_.jpg", "Best gender ever!");
+}
+
+function gen08() {
+  return new Gender("8", "big hairy kitty", "https://images-na.ssl-images-amazon.com/images/I/71mfn6d-G2L._SX679_.jpg", "Best gender ever!");
+}
